84d07e44d8a2f8e4cac72fc61be4a8677b7bbaac
thepoweroflove-cz <steph@aqnb.com>
2024-11-02 17:12:11 +0100

    Add files via upload

---
+++ old/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="sinb.css">
+<title>SINB</title>
+</head>
+<body>
+<div>
+<iframe src="https://player.vimeo.com/video/143782735?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		<iframe src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/125435230&amp&amp;inverse=false&amp;auto_play=true&amp;show_user=true" width="100%" height="20" frameborder="no" scrolling="no"></iframe></div>
+       
+   <div><script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us11.list-manage.com","uuid":"4400d58cc990f471370ee3ee1","lid":"ffa645b8d4"}) })</script>
+        </div>
+         <div><a href="http://stephkretowicz.com/brag"><IMG STYLE="position:absolute; TOP:350px; LEFT:800px; WIDTH:200px; HEIGHT:114px" SRC="public_html/images/somewhere ive nevr been.png" /></a></div>
+      
+</body>
+
+</html>
---
+++ old/public_html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="sinb.css">
+</head>
+<body>
+<iframe src="https://player.vimeo.com/video/143782735?autoplay=1&loop=1&title=0&byline=0&portrait=0" width="1500" height="843" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+</body>
+</html>
